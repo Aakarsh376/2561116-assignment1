a59fd569f8bf390cfdacd329305b72a83f717120
aakarsh376 <aakarsh376@gmail.com>
2026-07-21 11:18:52 +0530

    assignment completeted

---
+++ ug_3sem_ws/src/my_first_pkg_py/my_first_pkg_py/hello_ros.py
@@ -0,0 +1,5 @@
+def main():
+	print("System Check: Package my_first_pkg_py is active on ROS 2 Jazzy.")
+
+if __name__ == '__main__':
+	main()
---
+++ ug_3sem_ws/src/robot_info_py/robot_info_py/hello_ros.py
@@ -0,0 +1,5 @@
+def main():
+    print("System Check: Package robot_info_py is active on ROS 2 Jazzy.")
+
+if __name__ == '__main__':
+    main()
